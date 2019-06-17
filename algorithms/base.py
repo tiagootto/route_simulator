55b6dbf5192a86d2fd409f29fd5b399eb0c33339
tiagootto@hotmail.com <Pena10Fica>
2019-06-18 00:29:58 +0100

    added all files

---
+++ algorithms/base.py
@@ -0,0 +1,10 @@
+class BaseAlgorithm:
+
+    def __init__(self, map, ncars_by_street):
+        self.map = map
+        self.ncars_by_street = ncars_by_street
+
+    def get_route(self, source, dest):
+        pass
+
+
